6a90d5cf9379aaf3eb7f0b92fc24560e42d84072
root <root@raspberrypi.(none)>
2013-11-22 08:04:26 +0000

    config set

--- config/global.js
+++ config/global.js
@@ -15,8 +15,8 @@ config.habilitationCode = {
 // Sql configuration
 config.db_host = "localhost";
 config.db_port = 3306;
-config.db_user = "root";
-config.db_password = "root";
+config.db_user = "stimboard";
+config.db_password = "dews34Sd";
 config.db_name = "stimboard";
 
 
