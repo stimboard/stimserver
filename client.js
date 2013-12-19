70c92f290cde2d6e632fdaca8cc123ccdb078d34
Emmanuel Leveneur <emmanuelleveneur@banana.home>
2013-12-19 15:09:51 +0400

    Commented client.js, the client for test

--- client.js
+++ client.js
@@ -1,12 +1,20 @@
+/*
+*	Test client for tested the server
+*/
+
 var net = require('net');
 
+// Server Connection information
 var HOST = 'localhost';
 var PORT = 6969;
 
+// Create a new TCP socket
 var client = new net.Socket();
 
+// Connection to the server socket 
 client.connect(PORT, HOST, function() {
 	console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+	
 	// Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
 	client.write('29000628', 'UTF-8', function(){
 		console.log("Data has been sended ...");
@@ -22,4 +30,9 @@ client.on('data', function(data) {
 // Add a 'close' event handler for the client socket
 client.on('close', function() {
 	console.log('Connection closed');
+});
+
+// Error handler
+client.on('error',function(){
+    console.log("%j", arguments);
 });
